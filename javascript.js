ccf54c6d1dfbd8c062b06f3552122291ae46ddc8
Eric Thompson <thompsoneric3259@gmail.com>
2023-10-19 11:24:01 -0400

    Create compareChoices function
    compareChoices takes two string inputs representing player and computer choices of rock, paper, or scissors and compares them with nested switch statements. Depending on the choice, calls the win, lose, or tie function and returns null.

--- javascript.js
+++ javascript.js
@@ -14,3 +14,67 @@ function computerChoice() {
 function getRandomInt(n) {
     return Math.ceil(Math.random() * n);
 }
+
+function compareChoices(player, computer) {
+    switch (player) {
+        case 'rock':
+            switch (computer) {
+                case 'rock':
+                    tie();
+                    break;
+
+                case 'paper':
+                    lose();
+                    break;
+
+                case 'scissors':
+                    win();
+                    break;
+            }
+
+        case 'paper':
+            switch (computer) {
+                case 'rock':
+                    win();
+                    break;
+
+                case 'paper':
+                    tie();
+                    break;
+
+                case 'scissors':
+                    lose();
+                    break;
+            }
+
+        case 'scissors':
+            switch (computer) {
+                case 'rock':
+                    lose();
+                    break;
+
+                case 'paper':
+                    win();
+                    break;
+
+                case 'scissors':
+                    tie();
+                    break;
+            }
+
+        default:
+            throw 'Player choice not recognized';
+    }
+}
+
+function win() {
+
+}
+
+function lose() {
+
+}
+
+function tie() {
+
+}
